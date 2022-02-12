486adde6bdd27caf9f8204226089be792efcd993
dadler1 <d_adler@u.pacific.edu>
2022-02-12 14:33:32 -0800

    Added a regex check to ensure email is valid

--- Desktop Application/Screen.py
+++ Desktop Application/Screen.py
@@ -4,14 +4,35 @@ from re import *
 
 class Ticket_Screen:
     def __init__(self):
+        """===================================== CONFIGURATIONS ====================================="""
+
+        self.window_title = "Window"
+        self.resolution = "400x400"
+
+        """===================================== GUI COMPONENTS ====================================="""
+
         self.root = Tk()
+        self.root.title(self.window_title)
+        self.root.geometry(self.resolution)
         self.email_entry = Entry(self.root, width=50)
+        self.invalid_email_label = Label(self.root, width=50)
         self.ticket_title = Entry(self.root, width=50)
         # self.ticket_dropdown = OptionMenu(self.root)
         self.description_box = Entry(self.root, width=50)
         self.submission_button = Button(self.root, text="Submit", command=self.on_ticket_submission)
 
+    def draw_screen(self):
+        print("Put items on screen")
+
     def on_ticket_submission(self):
+        email = self.email_entry.get()
+        regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+        if not re.fullmatch(regex, email):
+            # Execute code to make invalid_email_label pop up
+            print("Invalid Email Address")
+        
+
+
         print("Perform Ticket Operations")
 
     def is_valid_email(self):
