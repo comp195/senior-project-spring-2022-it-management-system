83452218d45843bd506dcd8f1b6b1abec7c2f511
vcAndrew <a_vu16@u.pacific.edu>
2022-02-14 04:59:25 -0800

    Added sample code for connecting to and querying from MySQL Server

--- Desktop Application/test_screen.py
+++ Desktop Application/test_screen.py
@@ -1,6 +1,24 @@
 from tkinter import *
 from tkinter import ttk
 
+
+# CONNECTING TO MYSQL SERVER USING CONNECTOR/PYTHON
+# import mysql.connector
+# connection = mysql.connector.connect(user='user_here', password='password_here',
+#                                      host='111.1.1.1',
+#                                      database='employees')
+# connection.close()
+
+# QUERIES
+# connection = mysql.connector.connect(user='user_here', database='employees')
+# cursor = connection.cursor()
+# query = ("SELECT f_name, l_name, IT_status FROM employees "
+#          "WHERE IT_status = 1")
+# cursor.execute(query)
+# for listing in cursor:
+#     print(listing)
+
+
 root = Tk()
 frame = ttk.Frame(root, padding=400)
 frame.grid()
