accacbd6c04da43345b106c3262a0c760c56fb68
dadler1 <d_adler@u.pacific.edu>
2022-02-23 18:22:50 -0800

    added archetecture for page heirarchy

--- Desktop Application/Screen.py
+++ Desktop Application/Screen.py
@@ -1,103 +1,106 @@
 from playsound import playsound
-from tkinter import *
+import tkinter as tk
+from tkinter import font  as tkfont # python 3
 from re import *
 
+class GUIController(tk.Tk):
 
-class Ticket:
-    def __init__(self, email, title, category, description):
-        self.email = email
-        self.title = title
-        self.category = category
-        self.description = description
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
 
+        # Basic Configuration Values for Root Tk()
+        self.title_font = tkfont.Font(family='Helvetica', size=18, weight="bold", slant="italic")
+        self.window_title = "Application"
+        self.resolution = "1920x1080"
 
-class Ticket_Screen:
-    def __init__(self):
-        """===================================== CONFIGURATIONS ====================================="""
+        # Initializing container that stacks our frames
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
 
-        self.window_title = "Window"
-        self.resolution = "400x400"
-        self.dropdown_options = ["Computer Issue",
-                                 "Computer Upgrade",
-                                 "VoIP Phone Configuration",
-                                 "Equipment Request",
-                                 "Other"]
+        # Implementation of Root Tk() Configurations
+        self.title(self.window_title)
+        self.geometry(self.resolution)
+        self['bg'] = 'green'
 
-        """===================================== GUI COMPONENTS ====================================="""
+        # Initializing all of our frames within our container
+        self.frames = {}
+        for F in (PageHeader, MainMenuPage, EquipmentPage, EmployeePage, TicketPage, HelpPage):
+            page_name = F.__name__
+            frame = F(parent=container, controller=self)
+            self.frames[page_name] = frame
 
-        # Window
-        self.root = Tk()
-        self.root.title(self.window_title)
-        self.root.geometry(self.resolution)
+            # Putting all of our frames in the same place on the screen with the top one being active
+            frame.grid(row=0, column=0, sticky="nsew")
 
-        # Email Entry
-        self.email_label = Label(self.root, width=20, text="Email")
-        self.email_entry = Entry(self.root, width=50)
-        self.invalid_email_label = Label(self.root, width=50, text="Please enter a valid email.", fg="red")
+        # Frame visible at the start of the application
+        self.show_frame("PageHeader")
 
-        # Ticket Title Entry
-        self.title_label = Label(self.root, width=20, text="Title")
-        self.ticket_title = Entry(self.root, width=50)
-
-        # Ticket Category Dropdown
-        self.category_label = Label(self.root, width=20, text="Category")
-        self.dropdown_selection = StringVar(self.root)
-        self.dropdown_selection.set(self.dropdown_options[0])
-        self.ticket_dropdown = OptionMenu(self.root, self.dropdown_selection, self.dropdown_options[0], *self.dropdown_options[1:])
-
-        # Ticket Description Box
-        self.description_label = Label(self.root, width=20, text="Description")
-        self.description_box = Entry(self.root, width=50)
-
-        # Ticket Submission Button
-        self.submission_button = Button(self.root, text="Submit", command=self.on_ticket_submission)
-
-    def draw_screen(self):
-        print("Put items on screen")
-        self.email_label.pack()
-        self.email_entry.pack()
-        self.title_label.pack()
-        self.ticket_title.pack()
-        self.category_label.pack()
-        self.ticket_dropdown.pack()
-        self.description_label.pack()
-        self.description_box.pack()
-        self.submission_button.pack()
-
-    def setMaxWidth(self, element):
-        # f = tkFont.nametofont(element.cget("font"))
-        # zerowidth = f.measure("0")
-        # w = max([f.measure(i) for i in self.dropdown_options]) / zerowidth
-        # element.config(width=w)
-        print("Make it so that width of dropdown list is equal to longest element")
-
-    def on_ticket_submission(self):
-
-        email = self.email_entry.get()
-        regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
-        if not re.fullmatch(regex, email):
-            self.invalid_email_label.pack()
-            # Execute code to make invalid_email_label pop up and play error sound
-            # playsound("[Error Noise]")
-            print("Invalid Email Address")
-            return
-        self.invalid_email_label.destroy()
-        self.submission_button['state'] = 'disabled'
-        title = self.ticket_title.get()
-        category = self.dropdown_selection.get()
-        description = self.description_box.get()
-        ticket = Ticket(email, title, category, description)
-
-    def is_valid_email(self):
-        print("Use Regex to validate email in correct format")
+    
+    def show_frame(self, page_name):
+        '''Show a frame for the given page name'''
+        frame = self.frames[page_name]
+        frame.tkraise()
 
 
-def main():
-    ticket_screen = Ticket_Screen()
-    ticket_screen.draw_screen()
-    ticket_screen.root.mainloop()
-    #Testing git
+class PageHeader(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+
+        # Initializing GUI Controller
+        self.controller = controller
+        self.controller.config(menu=self.menubar)
+
+        # Setting Up the MenuBar
+        self.menubar = tk.Menu(master=controller)
+        self.menubar.add_command(label="Main Menu", command=lambda: self.controller.show_frame("MainMenuPage"))
+        self.menubar.add_command(label="Equipment", command=lambda: self.controller.show_frame("EquipmentPage"))
+        self.menubar.add_command(label="Employees", command=lambda: self.controller.show_frame("EmployeePage"))
+        self.menubar.add_command(label="Tickets", command=lambda: self.controller.show_frame("TicketPage"))
+        self.menubar.add_command(label="Help", command=lambda: self.controller.show_frame("HelpPage"))
+
+
+class MainMenuPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+
+        # Initializing GUI Controller
+        self.controller = controller
+
+
+class EquipmentPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+
+        # Initializing GUI Controller
+        self.controller = controller
+
+
+class EmployeePage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+
+        # Initializing GUI Controller
+        self.controller = controller
+
+
+class TicketPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+
+        # Initializing GUI Controller
+        self.controller = controller
+
+
+class HelpPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+
+        # Initializing GUI Controller
+        self.controller = controller
 
 
 if __name__ == "__main__":
-    main()
+    app = GUIController()
+    app.mainloop()
