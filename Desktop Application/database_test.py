6de55e70a1df45d917b2c98d85849b6274215d8d
vcAndrew <a_vu16@u.pacific.edu>
2022-02-25 21:31:54 -0800

    Added database connection error handling

--- Desktop Application/database_test.py
+++ Desktop Application/database_test.py
@@ -1,7 +1,18 @@
 import mysql.connector
+from mysql.connector import errorcode
 
-db = mysql.connector.connect(host="rds-management-system-mysql.c1frdktaj1ae.us-west-1.rds.amazonaws.com", user="admin",
-                             password="password")
+try:
+    db = mysql.connector.connect(host="rds-management-system-mysql.c1frdktaj1ae.us-west-1.rds.amazonaws.com",
+                                 user="admin", password="password")
+except mysql.connector.Error as db_err:
+    if db_err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Check login credentials!")
+    elif db_err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Confirm that database exists!")
+    else:
+        print(db_err)
+else:
+    db.close()
 
 print(db)
 print("hi")
