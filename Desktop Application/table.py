0eede0f6476b746a749133bc30151c6cef8f0b40
Vincent Tran <v_tran37@u.pacific.edu>
2022-03-05 03:49:18 -0800

    added insert_data function that can take a list of data and insert it into the table

--- Desktop Application/table.py
+++ Desktop Application/table.py
@@ -16,10 +16,10 @@ class dataTable:
         self.cursor.execute(check)
         result = self.cursor.fetchone()
         if result:
-            print("Table does exist")
+            print(name + " does exist")
             return True
         else:
-            print("Table does not exist")
+            print(name + " does not exist")
             return False
 
     def db_connect(self):
@@ -28,7 +28,7 @@ class dataTable:
                 host="rds-management-system-mysql.c1frdktaj1ae.us-west-1.rds.amazonaws.com",
                 user="admin", password="password", database='dbmanagementsystem')
             self.cursor = self.connection.cursor()
-            print("Connected", self.connection)
+            #print("Connected", self.connection)
         except mysql.connector.Error as db_err:
             if db_err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
                 print("Check login credentials!")
@@ -38,18 +38,60 @@ class dataTable:
                 print("ERROR:")
                 print(db_err)
 
-    def list_rows(self):
+    def get_rows(self):
         self.cursor.execute("SELECT * FROM dbmanagementsystem." + self.name)
         result = self.cursor.fetchall()
         # for i in result:
         #     print(i)
         return result
 
+    def get_cols(self):
+        self.cursor.execute("SHOW columns FROM dbmanagementsystem." + self.name)
+        result = self.cursor.fetchall()
+        col = []
+        for i in result:
+            col.append(i[0])
+        return col
+
+    def insert_data(self, data):
+        baseCMD = "INSERT INTO dbmanagementsystem." + self.name + " ("
+        # build cmd
+        col = self.get_cols()
+        j = 0
+        for i in col:
+            if j == len(col) - 1:
+                baseCMD += str(i) + ") VALUES ("
+            else:
+                baseCMD += str(i) + ", "
+            j += 1
+
+        j = 0
+        for i in data:
+            if j == len(data) - 1:
+                baseCMD += "'" + str(i) + "')"
+            else:
+                baseCMD += "'" + str(i) + "', "
+            j += 1
+
+        #print(baseCMD)
+        self.cursor.execute(baseCMD)
 
 def main():
-    test = dataTable("Tickets")
-    rows = test.list_rows()
-    print(rows)
+    tick = dataTable("Tickets")
+    print(tick.get_cols())
+
+    data = [2, "Broken Keyboard"]
+    tick.insert_data(data)
+
+    data2 = ["3", "Broken Mouse"]
+    tick.insert_data(data2)
+
+    print(tick.get_rows())
+
+    dev = dataTable("Devices")
+    data3 = ["1", "Monitor", "123", "Kawhi", "Leonard", "1", "Support", "365", "2021-03-05", "300.0"]
+    dev.insert_data(data3)
+    print(dev.get_rows())
 
 if __name__ == "__main__":
     main()
