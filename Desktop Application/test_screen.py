11757d8004607c0c99ad79915719182884e995ba
vcAndrew <a_vu16@u.pacific.edu>
2022-03-16 02:59:26 -0700

    Added details pane; set TreeView to act as part of main frame; formatted panes; fixed misaligned tabs

--- Desktop Application/test_screen.py
+++ Desktop Application/test_screen.py
@@ -1,3 +1,4 @@
+import tkinter
 import tkinter as tk
 from tkinter import *
 from tkinter.font import Font
@@ -14,7 +15,7 @@ stormcloud = "#4D646A"
 
 
 class MsgPanel(ttk.Frame):
-   def __init__(self, master, msgtxt):
+    def __init__(self, master, msgtxt):
       ttk.Frame.__init__(self, master)
       self.pack(side=TOP, fill=X)
 
@@ -24,12 +25,12 @@ class MsgPanel(ttk.Frame):
 
 
 class SeeDismissPanel(ttk.Frame):
-   def __init__(self, master):
-      ttk.Frame.__init__(self, master)
-      self.pack(side=BOTTOM, fill=X)  # resize with parent
-      # set resize constraints
-      self.rowconfigure(0, weight=1)
-      self.columnconfigure(0, weight=1)
+    def __init__(self, master):
+        ttk.Frame.__init__(self, master)
+        self.pack(side=BOTTOM, fill=X)  # resize with parent
+        # set resize constraints
+        self.rowconfigure(0, weight=1)
+        self.columnconfigure(0, weight=1)
 
 
 class GUIController(tk.Tk):
@@ -99,34 +100,46 @@ class PageHeader(tk.Frame):
 
 
 class MainPage(tk.Frame):
-   def __init__(self, parent, controller):
-      tk.Frame.__init__(self, parent)
-      # Initializing GUI Controller
-      self.controller = controller
-      self.search = SearchBarFrame(self, controller)
-      self.search.pack(side=tk.BOTTOM)
-      self.search_table = SearchFrame(self, controller)
-      self.search_table.pack(side=tk.BOTTOM)
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        # Initializing GUI Controller
+        self.controller = controller
+        self.search = SearchBarFrame(self, controller)
+        self.search.pack(side=tk.TOP)
+        self.search_table = SearchFrame(self, controller)
+        self.search_table.pack(side=tk.LEFT)
+        self.detail_frame = DetailFrame(self, controller)
+        self.detail_frame.pack(side=tk.RIGHT)
 
 
 class SearchBarFrame(tk.Frame):
-   def __init__(self, parent, controller):
-      tk.Frame.__init__(self, parent)
-      # Initializing GUI Controller
-      self.controller = controller
-      self.search_bar = tk.Entry(self, highlightbackground="#363030", highlightthickness=1,
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        # Initializing GUI Controller
+        self.controller = controller
+        self.search_bar = tk.Entry(self, highlightbackground="#363030", highlightthickness=1,
                               width=258)
-      self.search_bar.grid(row=0, column=0)
+        self.search_bar.grid(row=0, column=0)
 
-      self.search_space = tk.Label(self, width=10)
-      self.search_space.grid(row=0, column=1)
+        self.search_space = tk.Label(self, width=10)
+        self.search_space.grid(row=0, column=1)
 
-      self.search_button = tk.Button(self, text="Search",
+        self.search_button = tk.Button(self, text="Search",
                                       command=lambda: self.search())
-      self.search_button.grid(row=0, column=2)
+        self.search_button.grid(row=0, column=2)
 
-   def search(self):
-      return None
+    def search(self):
+        return None
+
+
+class DetailFrame(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        global SCREEN_WIDTH, SCREEN_HEIGHT, coconut, gainsboro, stormcloud
+        # Initializing GUI Controller
+        self.controller = controller
+        self.detail_frame = tk.Frame(self, bg="white", highlightbackground="#363030", highlightthickness=2, width=1300, height=670)
+        self.detail_frame.pack(side=tk.RIGHT)
 
 
 class SearchFrame(tk.Frame):
@@ -138,113 +151,109 @@ class SearchFrame(tk.Frame):
         # TODO
         self.search_grid = MCListDemo(self)
         self.search_grid.pack()
-
-
-class DetailFrame(tk.Frame):
-   def __init__(self, parent, controller):
-      tk.Frame.__init__(self, parent)
-      global SCREEN_WIDTH, SCREEN_HEIGHT, coconut, gainsboro, stormcloud
-      # Initializing GUI Controller
-      self.controller = controller
+        # self.search_grid.pack(side=tk.LEFT)
 
 
 class MCListDemo(ttk.Frame):
-   # class variable to track direction of column
-   # header sort
-   SortDir = True  # descending
+    # class variable to track direction of column
+    # header sort
+    SortDir = True  # descending
 
-   def __init__(self, isapp=True, name='mclistdemo'):
-      ttk.Frame.__init__(self, name=name)
-      self.pack(expand=Y, fill=BOTH)
-      self.isapp = isapp
-      self._create_widgets()
+    # def __init__(self, isapp=True, name='mclistdemo'):
+    def __init__(self, parent, isapp=True, name='mclistdemo'):
+        # ttk.Frame.__init__(self, name=name)
+        ttk.Frame.__init__(self, parent, name=name)
+        self.pack(expand=Y, fill=BOTH)
+        # self.pack(side=tk.LEFT)
+        self.isapp = isapp
+        self._create_widgets()
 
-   def _create_widgets(self):
-      if self.isapp:
-         SeeDismissPanel(self)
+    def _create_widgets(self):
+        if self.isapp:
+            SeeDismissPanel(self)
 
-      self._create_demo_panel()
+        self._create_demo_panel()
 
-   def _create_demo_panel(self):
-      demoPanel = Frame(self)
-      demoPanel.pack(side=TOP, fill=BOTH, expand=Y)
+    def _create_demo_panel(self):
+        demoPanel = Frame(self)
+        demoPanel.pack(side=TOP, fill=BOTH, expand=Y)
+        # demoPanel.pack(side=tk.LEFT)
 
-      self._create_treeview(demoPanel)
-      self._load_data()
+        self._create_treeview(demoPanel)
+        self._load_data()
 
-   def _create_treeview(self, parent):
-      f = ttk.Frame(parent)
-      f.pack(side=TOP, fill=BOTH, expand=Y)
+    def _create_treeview(self, parent):
+        f = ttk.Frame(parent)
+        f.pack(side=TOP, fill=BOTH, expand=Y)
+        # f.pack(side=tk.LEFT)
 
-      # create the tree and scrollbars
-      self.dataCols = ('country', 'capital', 'currency')
-      self.tree = ttk.Treeview(columns=self.dataCols,
-                               show='headings')
+        # create the tree and scrollbars
+        self.dataCols = ('country', 'capital', 'currency')
+        self.tree = ttk.Treeview(columns=self.dataCols,
+                               show='headings', height=31)
 
-      ysb = ttk.Scrollbar(orient=VERTICAL, command=self.tree.yview)
-      xsb = ttk.Scrollbar(orient=HORIZONTAL, command=self.tree.xview)
-      self.tree['yscroll'] = ysb.set
-      self.tree['xscroll'] = xsb.set
+        ysb = ttk.Scrollbar(orient=VERTICAL, command=self.tree.yview)
+        xsb = ttk.Scrollbar(orient=HORIZONTAL, command=self.tree.xview)
+        self.tree['yscroll'] = ysb.set
+        self.tree['xscroll'] = xsb.set
 
-      # add tree and scrollbars to frame
-      self.tree.grid(in_=f, row=0, column=0, sticky=NSEW)
-      ysb.grid(in_=f, row=0, column=1, sticky=NS)
-      xsb.grid(in_=f, row=1, column=0, sticky=EW)
+        # add tree and scrollbars to frame
+        self.tree.grid(in_=f, row=0, column=0, sticky=NSEW)
+        ysb.grid(in_=f, row=0, column=1, sticky=NS)
+        xsb.grid(in_=f, row=1, column=0, sticky=EW)
 
-      # set frame resize priorities
-      f.rowconfigure(0, weight=1)
-      f.columnconfigure(0, weight=1)
+        # set frame resize priorities
+        f.rowconfigure(0, weight=1)
+        f.columnconfigure(0, weight=1)
 
-   def _load_data(self):
+    def _load_data(self):
+        self.data = [
+            ("Argentina", "Buenos Aires", "ARS"),
+            ("Australia", "Canberra", "AUD"),
+            ("Brazil", "Brazilia", "BRL"),
+            ("Canada", "Ottawa", "CAD"),
+            ("China", "Beijing", "CNY"),
+            ("France", "Paris", "EUR"),
+            ("Germany", "Berlin", "EUR"),
+            ("India", "New Delhi", "INR"),
+            ("Italy", "Rome", "EUR"),
+            ("Japan", "Tokyo", "JPY"),
+            ("Mexico", "Mexico City", "MXN"),
+            ("Russia", "Moscow", "RUB"),
+            ("South Africa", "Pretoria", "ZAR"),
+            ("United Kingdom", "London", "GBP"),
+            ("United States", "Washington, D.C.", "USD")]
 
-      self.data = [
-         ("Argentina", "Buenos Aires", "ARS"),
-         ("Australia", "Canberra", "AUD"),
-         ("Brazil", "Brazilia", "BRL"),
-         ("Canada", "Ottawa", "CAD"),
-         ("China", "Beijing", "CNY"),
-         ("France", "Paris", "EUR"),
-         ("Germany", "Berlin", "EUR"),
-         ("India", "New Delhi", "INR"),
-         ("Italy", "Rome", "EUR"),
-         ("Japan", "Tokyo", "JPY"),
-         ("Mexico", "Mexico City", "MXN"),
-         ("Russia", "Moscow", "RUB"),
-         ("South Africa", "Pretoria", "ZAR"),
-         ("United Kingdom", "London", "GBP"),
-         ("United States", "Washington, D.C.", "USD")]
+        # configure column headings
+        for c in self.dataCols:
+             self.tree.heading(c, text=c.title(),
+                               command=lambda c=c: self._column_sort(c, MCListDemo.SortDir))
+             self.tree.column(c, width=Font().measure(c.title()))
 
-      # configure column headings
-      for c in self.dataCols:
-         self.tree.heading(c, text=c.title(),
-                           command=lambda c=c: self._column_sort(c, MCListDemo.SortDir))
-         self.tree.column(c, width=Font().measure(c.title()))
+        # add data to the tree
+        for item in self.data:
+            self.tree.insert('', 'end', values=item)
 
-      # add data to the tree
-      for item in self.data:
-         self.tree.insert('', 'end', values=item)
+        # and adjust column widths if necessary
+        for idx, val in enumerate(item):
+            width = Font().measure(val)
+            if self.tree.column(self.dataCols[idx], 'width') < width:
+                self.tree.column(self.dataCols[idx], width=width)
 
-         # and adjust column widths if necessary
-         for idx, val in enumerate(item):
-            iwidth = Font().measure(val)
-            if self.tree.column(self.dataCols[idx], 'width') < iwidth:
-               self.tree.column(self.dataCols[idx], width=iwidth)
+    def _column_sort(self, col, descending=False):
+        # grab values to sort as a list of tuples (column value, column id)
+        # e.g. [('Argentina', 'I001'), ('Australia', 'I002'), ('Brazil', 'I003')]
+        data = [(self.tree.set(child, col), child) for child in self.tree.get_children('')]
 
-   def _column_sort(self, col, descending=False):
+        # reorder data
+        # tkinter looks after moving other items in
+        # the same row
+        data.sort(reverse=descending)
+        for indx, item in enumerate(data):
+            self.tree.move(item[1], '', indx)  # item[1] = item Identifier
 
-      # grab values to sort as a list of tuples (column value, column id)
-      # e.g. [('Argentina', 'I001'), ('Australia', 'I002'), ('Brazil', 'I003')]
-      data = [(self.tree.set(child, col), child) for child in self.tree.get_children('')]
-
-      # reorder data
-      # tkinter looks after moving other items in
-      # the same row
-      data.sort(reverse=descending)
-      for indx, item in enumerate(data):
-         self.tree.move(item[1], '', indx)  # item[1] = item Identifier
-
-      # reverse sort direction for next sort operation
-      MCListDemo.SortDir = not descending
+        # reverse sort direction for next sort operation
+        MCListDemo.SortDir = not descending
 
 if __name__ == "__main__":
     app = GUIController()
